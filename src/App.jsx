d4e5507b1f9222092ddc6d39e922a82efd382b47
owenizedd@gmail.com <ryan.owen@bukalapak.com>
2021-06-11 20:25:39 +0700

    Initial Commit & Setup Project & Router & Material UI

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText'
+import { makeStyles, useTheme } from '@material-ui/core/styles'; 
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+
+function App() {
+  const sidebarItems = [
+    {
+      name: 'Home',
+      to: '/'
+    },
+    {
+      name: 'View Products',
+      to: '/products'
+    },
+    {
+      name: 'Insert Product',
+      to: '/products/add'
+    }, 
+  ];
+
+  return (
+    <div className="App">
+      <Router>
+        <nav className='sidebar'>
+          <List>
+            {
+              sidebarItems.map((item, index) => (
+                <Link to={item.to} key={item.name}>
+                  <ListItem button>
+                    {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
+                    
+                    <ListItemText primary={item.name} />
+                  </ListItem>
+                </Link>
+              ))
+            }
+          </List>
+        </nav>
+        <div className="page-margin"></div>
+
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/products" exact>
+            <Products/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home(){
+  return(
+    <div className="home">
+      My Home
+    </div>
+  )
+}
+
+
+export default Home;
